b94ed2946376d52348926e40700be5eabc43e943
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-02-02 16:34:16 +0100

    added plot and slider to map

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
 
@@ -32,10 +33,21 @@ def get_options(list_countries):
     return dict_list
 
 # df_country = df_country.set_index('date').groupby(pd.Grouper(freq='2D')).sum().reset_index()
+df_population = df[df['date']==df['date'].max()].reset_index(drop=True)
+
+scatter_hdi = px.scatter(df_population, x="gdp_per_capita", y="human_development_index",
+                 size="population", color="location",
+                 hover_name="location", log_x=True, size_max=60,
+                 title="Variation du GDP en fonction du HDI")
+
+#----------------------------------------------------------------------------
 
 app.layout = html.Div(children=[
     html.H1(children='Covid-19 Data Visualisation'),
-        
+    html.Div(children=[
+        #html.P("GDP en fonction du HDI"),
+        dcc.Graph(id="scatter_hdi", figure=scatter_hdi),
+        ]),
     html.Div(children=[
         html.P("Sélectionner pays :"),
         dcc.Dropdown(id='country-filter', options=get_options(df['location'].unique()),
@@ -49,7 +61,7 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.RadioItems(id='feature', 
         options=[{'value': x, 'label': x} 
-                 for x in ['total_cases', 'total_deaths','population']],
+                 for x in ['total_cases', 'total_deaths']],
         value='total_cases',
         labelStyle={'display': 'inline-block'}
     ),
@@ -94,20 +106,30 @@ def update_graph(selected_dropdown_value):
 @app.callback(Output("choropleth", "figure"), 
               [Input("feature", "value")])
 def display_choropleth(feature):
-    df_population = df[df['date']==df['date'].max()].reset_index(drop=True)
-    worldmap = dict(type='choropleth',
-            locations=df_population['location'],
-            locationmode='country names',
-            text=df_population['location'],
-            z=df_population[feature],
-            colorbar_title = feature
-            )
+    # worldmap = dict(type='choropleth',
+    #         locations=df_population['location'],
+    #         locationmode='country names',
+    #         text=df_population['location'],
+    #         z=df_population[feature],
+    #         colorbar_title = feature
+    #         )
     
-    layout_map = dict(geo = dict(scope='world', showlakes= False), 
-                      title="Nombre de cas et de morts de la covid-19 dans le monde")
-    choromap = go.Figure(data=[worldmap], layout=layout_map)
-
-    return choromap
+    # layout_map = dict(geo = dict(scope='world', showlakes= False), 
+    #                   title="Nombre de cas et de morts de la covid-19 dans le monde")
+    # choromap = go.Figure(data=[worldmap], layout=layout_map)
+    
+    
+    df_dates = df.groupby([df.date.dt.strftime('%m %Y'), 'location','iso_code'])[['total_cases', 'total_deaths', 'population', 'gdp_per_capita', 'human_development_index']].max().reset_index()
+    df_dates['date'] = pd.to_datetime(df_dates['date'])
+    df_dates = df_dates.sort_values(by='date',ascending=True).reset_index(drop=True)
+    df_dates['date'] = df_dates['date'].astype(str)
+    fig = px.choropleth(df_dates, locations="iso_code",
+                    color=feature, 
+                    hover_name="location",
+                    animation_frame="date",
+                    animation_group="location")
+    fig["layout"].pop("updatemenus") # optional, drop animation buttons
+    return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
