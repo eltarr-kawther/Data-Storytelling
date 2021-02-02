68f4d3031c01198f8f23076186b40840d6a405d6
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-02-02 11:12:14 +0100

    working worldmap callback ~

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 import pandas as pd
-import numpy as np
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 #external_stylesheets=external_stylesheets
@@ -19,7 +18,7 @@ app = dash.Dash(__name__)
 
 server = app.server
 
-app.title = "Coviz"
+app.title = "CoViz"
 
 df = pd.read_csv('raw_data.csv', sep=',')
 df = df.drop(['Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11',
@@ -32,27 +31,7 @@ def get_options(list_countries):
         dict_list.append({'label': c, 'value': c})
     return dict_list
 
-
-# country='France'
-
-# df_country = df[df['location']==country]
-
 # df_country = df_country.set_index('date').groupby(pd.Grouper(freq='2D')).sum().reset_index()
-
-df_population = df[df['date']==df['date'].max()].reset_index(drop=True)
-
-worldmap = dict(type='choropleth',
-            locations=df_population['location'],
-            locationmode='country names',
-            text=df_population['location'],
-            z=df_population['total_cases'],
-            colorbar_title = "Total cases"
-            )
-
-layout_map = dict(geo = dict(scope='world',
-                         showlakes= False), 
-                  title="Nombre de cas de la covid-19 dans le monde")
-choromap = go.Figure(data=[worldmap], layout=layout_map)
 
 app.layout = html.Div(children=[
     html.H1(children='Covid-19 Data Visualisation'),
@@ -67,10 +46,15 @@ app.layout = html.Div(children=[
             dcc.Graph(id='covid-evolution',config={'displayModeBar': False}, animate=True)
         ]),
     
-    html.Div(className='World-map',
-              children=[
-        dcc.Graph(id='graph2', figure=choromap)
-        ])
+    html.Div([
+        dcc.RadioItems(id='feature', 
+        options=[{'value': x, 'label': x} 
+                 for x in ['total_cases', 'total_deaths','population']],
+        value='total_cases',
+        labelStyle={'display': 'inline-block'}
+    ),
+    dcc.Graph(id="choropleth"),
+])
 ])
              
 #----------------------------------------------------------------------------
@@ -107,6 +91,23 @@ def update_graph(selected_dropdown_value):
               }
     return figure
 
+@app.callback(Output("choropleth", "figure"), 
+              [Input("feature", "value")])
+def display_choropleth(feature):
+    df_population = df[df['date']==df['date'].max()].reset_index(drop=True)
+    worldmap = dict(type='choropleth',
+            locations=df_population['location'],
+            locationmode='country names',
+            text=df_population['location'],
+            z=df_population[feature],
+            colorbar_title = feature
+            )
+    
+    layout_map = dict(geo = dict(scope='world', showlakes= False), 
+                      title="Nombre de cas et de morts de la covid-19 dans le monde")
+    choromap = go.Figure(data=[worldmap], layout=layout_map)
+
+    return choromap
+
 if __name__ == '__main__':
     app.run_server(debug=False)
-    
